7c47ef85578dc0f5ecda98f41a7e056f32d9eed6
Baldyl66 <dylanbalmigere@gmail.com>
2026-07-20 21:40:43 +0200

    5

--- deploy-bot.bat
+++ deploy-bot.bat
@@ -9,7 +9,7 @@ git commit -m "%msg%"
 git push
 
 echo.
-ssh -i "C:\Users\dylan\Desktop\bot-lcp-discord\infos-serveur\ssh-key-2026-03-14.key" ubuntu@89.168.60.237 "cd ~/bot-lcp-discord && git pull && npm install && pm2 restart lcp-bot && cd backend && npm install && npm run build && (pm2 restart virtual-world-backend || pm2 start dist/index.js --name virtual-world-backend) && cd ../frontend && npm install && npm run build"
+ssh -i "C:\Users\dylan\Desktop\bot-lcp-discord\infos-serveur\ssh-key-2026-03-14.key" ubuntu@89.168.60.237 "cd ~/bot-lcp-discord && git stash && git pull && git stash pop && npm install && pm2 restart lcp-bot && cd backend && npm install && npm run build && (pm2 restart virtual-world-backend || pm2 start dist/index.js --name virtual-world-backend) && cd ../frontend && npm install && npm run build"
 
 echo.
 echo Deploiement termine.
